bde2bd43c76568f1e9df8ff009ddf5ee27d91260
kellykkhoo <103315546+kellykkhoo@users.noreply.github.com>
2024-08-21 11:01:02 +0800

    Update app.py

--- BC3415/app.py
+++ BC3415/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 import google.generativeai as palm 
 import os
 
-#api = "AIzaSyCNYWXiaRU1zUpf6w08yrvHjMqaV77r3R8" cannot post api keys 
 api = os.getenv("MAKERSUITE_API_TOKEN") 
 palm.configure(api_key=api)
 model = {"model": "models/chat-bison-001"}
